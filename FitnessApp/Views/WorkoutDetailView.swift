e6fc8b34171ba2999a5668a93e96255b1faade98
32tarcan <tarcann.bahadir@gmail.com>
2024-08-01 18:06:32 +0300

    Asset folder changed.

---
+++ FitnessApp/Views/WorkoutDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutDetailView.swift
+//  FitnessApp
+//
+//  Created by Sakans on 1.08.2024.
+//
+
+import SwiftUI
+
+struct WorkoutDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutDetailView()
+}
