91af51ef2fd155ddad587b5be0b0a47f0d02630d
32tarcan <tarcann.bahadir@gmail.com>
2024-08-02 15:12:14 +0300

    WebWiew Helper added.

--- FitnessApp/Helpers/WebWiew.swift
+++ FitnessApp/Helpers/WebWiew.swift
@@ -5,4 +5,20 @@
 //  Created by Sakans on 2.08.2024.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
