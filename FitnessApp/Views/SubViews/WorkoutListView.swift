9a749d6b6ccd34309e99da979c048e9d69cdbaf6
32tarcan <tarcann.bahadir@gmail.com>
2024-08-20 02:02:10 +0300

    List View added for fetching.

---
+++ FitnessApp/Views/SubViews/WorkoutListView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutListView.swift
+//  FitnessApp
+//
+//  Created by Sakans on 20.08.2024.
+//
+
+import SwiftUI
+
+struct WorkoutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutListView()
+}
