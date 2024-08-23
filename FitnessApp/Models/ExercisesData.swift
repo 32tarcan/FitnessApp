1188b0101dac0edb23209d453d73de4a248c60f6
32tarcan <tarcann.bahadir@gmail.com>
2024-08-23 17:12:49 +0300

    Exercises Data demo file created.

---
+++ FitnessApp/Models/ExercisesData.swift
@@ -0,0 +1,15 @@
+//
+//  ExercisesData.swift
+//  FitnessApp
+//
+//  Created by Sakans on 23.08.2024.
+//
+
+import Foundation
+
+struct ExercisesData: Identifiable {
+    var id: String
+    var title: String
+    var gif: String
+}
+
