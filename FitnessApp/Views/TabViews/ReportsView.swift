b3bd07ec1d88c46797e55c1b1b728c65545d0689
32tarcan <tarcann.bahadir@gmail.com>
2024-08-28 17:33:04 +0300

    Reports View architacture MVVM adding.

--- FitnessApp/Views/TabViews/ReportsView.swift
+++ FitnessApp/Views/TabViews/ReportsView.swift
@@ -8,30 +8,28 @@
 import SwiftUI
 
 struct ReportsView: View {
-    @State private var selectedDates: Set<Date> = []
-    @State private var currentMonth: Date = Date()
-    @Environment(\.calendar) var calendar
+    @StateObject private var viewModel = ReportsViewModel()
     @EnvironmentObject var authViewModel: AuthViewModel
 
     var body: some View {
         VStack {
             Text("Don't Break The Chain!")
-                .padding(.top, -40)
+                .padding(.top, -10)
                 .font(.largeTitle)
                 .foregroundColor(Color(hex: "1E8FB2"))
             
             HStack {
                 Button(action: {
-                    changeMonth(by: -1)
+                    viewModel.changeMonth(by: -1)
                 }) {
                     Image(systemName: "arrowshape.left")
                         .foregroundColor(Color(hex: "1E8FB2"))
                 }
                 Spacer()
-                Text("\(formattedMonth(currentMonth))")
+                Text("\(viewModel.formattedMonth(viewModel.currentMonth))")
                 Spacer()
                 Button(action: {
-                    changeMonth(by: 1)
+                    viewModel.changeMonth(by: 1)
                 }) {
                     Image(systemName: "arrowshape.right")
                         .foregroundColor(Color(hex: "1E8FB2"))
@@ -40,11 +38,11 @@ struct ReportsView: View {
             .padding()
 
             LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
-                ForEach(generateDaysForMonth(), id: \.self) { date in
-                    Text("\(calendar.component(.day, from: date))")
+                ForEach(viewModel.generateDaysForMonth(), id: \.self) { date in
+                    Text("\(viewModel.calendar.component(.day, from: date))")
                         .frame(width: 40, height: 40)
                         .foregroundColor(.white)
-                        .background(isDateSelected(date) ? Color(hex: "1E8FB2").opacity(0.7) : Color.clear)
+                        .background(viewModel.isDateSelected(date) ? Color(hex: "1E8FB2").opacity(0.7) : Color.clear)
                         .cornerRadius(40)
                 }
             }
@@ -70,54 +68,9 @@ struct ReportsView: View {
         }
         .onAppear {
             if let lastLoginDate = authViewModel.lastLoginDate {
-                addDateToSelected(lastLoginDate)
+                viewModel.addDateToSelected(lastLoginDate)
             }
-            addTodayIfNeeded()
-        }
-    }
-
-    func changeMonth(by value: Int) {
-        if let newMonth = calendar.date(byAdding: .month, value: value, to: currentMonth) {
-            currentMonth = newMonth
-        }
-    }
-
-    func formattedMonth(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM yyyy"
-        return formatter.string(from: date)
-    }
-    
-    func formattedDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .short
-        return formatter.string(from: date)
-    }
-
-    func generateDaysForMonth() -> [Date] {
-        guard let monthInterval = calendar.dateInterval(of: .month, for: currentMonth) else {
-            return []
-        }
-
-        let days = calendar.dateComponents([.day], from: monthInterval.start, to: monthInterval.end).day!
-        return (0..<days).compactMap {
-            calendar.date(byAdding: .day, value: $0, to: monthInterval.start)
-        }
-    }
-    
-    func isDateSelected(_ date: Date) -> Bool {
-        return selectedDates.contains { calendar.isDate($0, inSameDayAs: date) }
-    }
-
-    func addDateToSelected(_ date: Date) {
-        selectedDates.insert(date)
-    }
-    
-    func addTodayIfNeeded() {
-        let today = Date()
-        if !selectedDates.contains { calendar.isDate($0, inSameDayAs: today) } {
-            selectedDates.insert(today)
+            viewModel.addTodayIfNeeded()
         }
     }
 }
